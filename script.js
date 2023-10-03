16cb67c90bd1027db3e77537cf72f83fafa40c01
QuerulousEggman <ultimofantasista10@gmail.com>
2023-10-03 21:04:34 +0100

    updated0310

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
-// function myFunction() {
-//   let x = document.getElementById('myTopnav');
-//   if (x.className === 'login') {
-//     x.className += 'responsive';
-//   } else {
-//     x.className = 'login';
-//   }
-// }
+addEventListener('click', (event) => {});
+
+onclick = (event) => {};
